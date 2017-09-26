96a3ffb9c209bb4546ebbce8d868802317ef4508
David Luhrsen <davidluhrsen@outlook.com>
2017-09-26 11:23:17 -0700

    Removed script from index and put in sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,32 @@
-$(document).ready(function () {
-    for (var i = 0; i <= 10; i++) {
-        $('<div />', {
-            'class' : 'sameDiv',
-            'id'    : 'div' + i
-        }).appendTo('body');
-    }
+var grid = 16;
+var cell = 600/grid;
+
+$(document).ready(function() {
+  drawBoard();
+
+ $('.clear').click(newBoard);
+
 });
+
+
+function drawBoard() {
+  $('#container').empty();
+  cell = 600/grid;
+    for(var x = 0; x < grid; x++) {
+        for(var y = 0; y < grid; y++) {
+            var unit = $("<div class='unit'></div>");
+            unit.css({'width':cell+'px', 'height':cell + 'px'});
+            unit.appendTo('#container');
+        }
+    }
+    $('.unit').hover(function() {
+        $(this).css('background-color', '#252b30');
+        });
+
+};
+
+function newBoard(){
+   $('.unit').css("background-color", "white");
+   grid = prompt("what size grid would you like?");
+   drawBoard();
+ }
